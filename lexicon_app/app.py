3a880b3a982588517a585508b0eca2a7307f7d5b
Milk T <milkt2021@gmail.com>
2024-02-09 15:11:02 +0100

    first commit

---
+++ lexicon_app/app.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
